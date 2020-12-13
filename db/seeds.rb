51fb40c999839e11a7fc9e9f455fe2f0b045ba28
smolnreadytobrawl <noelani.villanueva@gmail.com>
2020-12-13 09:10:01 -0800

    create the seeds to see the progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: "Mark", last_name: "Aruleus")
